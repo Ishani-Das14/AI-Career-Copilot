61649c15aa2a9411fc34cda89539c4cd4649297f
Ishani-Das14 <iishaaani@gmail.com>
2026-07-08 13:00:31 +0530

    Integrate Gemini API
    Configured Gemini API, loaded the API key securely from the .env file, and made the first successful LLM call using LangChain.

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=api_key
+)
+
+response = model.invoke("tell me somethingn about cricket .")
+
+print(response.content) 
